d4035129c8088f14a04e0749916866e13decb063
Fabrice Flore-Thébault <243761+themr0c@users.noreply.github.com>
2020-01-24 10:07:54 +0100

    devconf.cz 2020

---
+++ 2020-01-efficient-downstreaming/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bundle config --local github.https true
+bundle --path=.bundle/gems --binstubs=.bundle/.bin
+git clone -b 3.7.0 --depth 1 https://github.com/hakimel/reveal.js.git
+npm install highlight.js
+
+
